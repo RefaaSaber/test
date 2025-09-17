d0d5ffe276fdb49c81016fa5b99774a77f39aae7
RefaaSaber <rhusainsaber@stu.kau.edu.sa>
2025-09-17 07:25:05 +0300

    Delete CPIT305-GroupProject2-main directory

--- CPIT305-GroupProject2-main/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.cpit.project {
-    
-    requires javafx.controls;
-    requires javafx.fxml; 
-    requires de.jensd.fx.glyphs.fontawesome;
-    opens com.mycompany.inventoryfx to javafx.fxml, javafx.graphics;
-}
